cf8c9995e212475b21749105e79324d43312e615
Daniels Auzins <73557869+DanielsATech@users.noreply.github.com>
2022-05-15 20:41:29 +0100

    Write Function
    Simple Write function to list data from the scraper into a txt called parserdata4

--- scraper.py
+++ scraper.py
@@ -3,9 +3,23 @@ from bs4 import BeautifulSoup
 
 
 
+
 resq = requests.get('https://en.wikipedia.org/wiki/Python.html')
 print("Status code is", resq.status_code)
 
 data = BeautifulSoup(resq.text, "html5lib")
 print(data.title)
-print(data.find_all('div'))
+
+
+datavar = data.find_all("div")
+
+
+
+def WriteData():
+    fi = open("parserdata4.txt", "x")
+    fi.write(str(datavar))
+    fi.close()
+    print("Txt has been made")
+
+WriteData()
+
